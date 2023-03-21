47f930872945fe82953b32df86311aadf2c2f997
Anchal Gupta <anchal.gupta@w3villa.com>
2023-03-22 00:25:50 +0530

    Added random background color to panda app

--- src/components/main/child.js
+++ src/components/main/child.js
@@ -1,38 +1,62 @@
-import React, { useEffect } from 'react'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import './child.css'
+export default function Child({ child }) {
+  var color = [
+    "lightblue",
+    "lightcyan",
+    "lightgoldenrodyellow",
+    "lightgrey",
+    "lightyellow",
+    "lightpink",
+    "mistyrose",
+    "moccasin",
+    "oldlace",
+    "palegoldenrod",
+    "papayawhip",
+    "wheat",
+    "whitesmoke",
+    "mintcream",
+    "lightsteelblue",
+    "lightsalmon",
+    "khaki",
+    "antiquewhite"
+  ];
 
-export default function Child({child}) {
-
-    useEffect(() => {
-        if(child) {
-            toast("Congratulations, Panda have " + (child) + " children");
-        }
-    }, [child]);
-
-    useEffect(() => {
-      toast.info("Welcome to my family, I am Panda Parent!")
-    }, [])
-    
-    
-
-    const createChild = (child) => {
-        var children = [];
-        for (let i = 0; i < child; i++) {
-            children.push(<img src="/image/pandachild.png" alt="pandaChild" width="200px" height="200px" />)
-        }
-        return children;
+  useEffect(() => {
+    if (child) {
+      toast("Congratulations, Panda have " + child + " children");
     }
+    document.body.style.background =
+      color[Math.floor(Math.random() * color.length)];
+  }, [child]);
+
+  useEffect(() => {
+    toast.info("Welcome to my family, I am Panda Parent!");
+  }, []);
+
+  const createChild = (child) => {
+    var children = [];
+    for (let i = 0; i < child; i++) {
+      children.push(
+        <img
+          className="zoom"
+          src="/image/pandachild.png"
+          alt="pandaChild"
+          width="200px"
+          height="200px"
+        />
+      );
+    }
+    return children;
+  };
 
   return (
     <div class="border border-3 border-success m-5">
-      <div>
-        {createChild(child)}
-      </div>
-      <div class="badge text-bg-warning fs-5 m-4">
-        I have {child} child
-      </div>
+      <div>{createChild(child)}</div>
+      <div class="badge text-bg-warning fs-5 m-4">Panda has {child} child</div>
       <ToastContainer />
     </div>
-  )
+  );
 }
